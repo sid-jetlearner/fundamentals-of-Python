0ca1caa9c1e8ef00e08fcc55edf34a0972fcc356
sid-jetlearner <sidharthaadya@gmail.com>
2025-10-11 19:04:16 +0100

    Add files via upload

---
+++ project 6.1.py
@@ -0,0 +1,12 @@
+print('calculator')
+x=int(input('enter a number'))
+y=int(input('enter a number'))
+op=input("please select + - / *")
+if op=='+':
+    print(x+y)
+elif op=='-':
+    print(x-y)
+elif op=='*':
+    print(x*y)
+elif op=='/':
+    print(x/y)
---
+++ project 6.py
@@ -0,0 +1,21 @@
+#arithmetic, logical, and comparison operators in Python
+x=11
+y=2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(2**3)
+z="hello"
+print(z+"sid")
+print(z*4)
+print(x>y)
+print(x>=11)
+print(x<=10)
+print(x==10)
+print(y!=10)
+print(x==11 and y!=10)
+print(x==10 or y!=10)
+print(not x==10)
