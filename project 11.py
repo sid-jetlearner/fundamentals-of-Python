c08192603bdfdfdf55e84783fc1552fbecdea111
sid-jetlearner <sidharthaadya@gmail.com>
2026-01-17 18:49:48 +0000

    Add files via upload

---
+++ project 11.py
@@ -0,0 +1,15 @@
+# i=0 
+# while i!=100:
+#     print(i)
+#     i=i+1
+# a=int(input("Enter any number of your choice: "))
+# if a%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+# x=10
+# def my_function():
+#     x=5
+#     print(x)
+# my_function()
+# print(x)
